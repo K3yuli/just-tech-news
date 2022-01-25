ecfbdbeb2380c9f2e50cce29bc37de650452136b
Kenia Lizama-Gamez <ke.yulizama@gmail.com>
2022-01-25 12:14:42 -0500

    user route

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -41,17 +41,18 @@ router.get('/:id', (req, res) => {
 
 // POST /api/users
 router.post('/', (req, res) => {
-    // to insert data use sequelize's .create() method
+// to insert data use sequelize's .create() method
+// expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
     User.create({
-        username: res.body.username,
+        username: req.body.username,
         email: req.body.email,
         password: req.body.password
     })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
         console.log(err);
         res.status(500).json(err);
-    })
+        });
 });
 
 // PUT /api/users/1
