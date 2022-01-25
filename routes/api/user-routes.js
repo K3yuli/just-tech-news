481b2259c6dc96349dd96f9910f3cc10e7f8a252
Kenia Lizama-Gamez <ke.yulizama@gmail.com>
2022-01-25 12:04:06 -0500

    user code

---
+++ routes/api/user-routes.js
@@ -0,0 +1,100 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// GET /api/users
+router.get('/', (req, res) => {
+    // access our User model and run .findAll() method)
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// GET /api/users/1
+router.get('/:id', (req, res) => {
+    // passing an argument into the .findOne method
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        }
+    })
+    // if the then method received nothing from the query res with 404 - indicating that everything is okay the user just asked for the wrong piece of data
+    .then(dbUserData => {
+        if(!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// **to update existing data, use both req.body and req.params**
+
+// POST /api/users
+router.post('/', (req, res) => {
+    // to insert data use sequelize's .create() method
+    User.create({
+        username: res.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+
+// PUT /api/users/1
+router.put('/:id', (req, res) => {
+// if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+// pass req.body to provide the new data we want to use in the update
+    User.update(req.body, {
+        where: {
+            // pass req.params.id to indicate where exactly we want the new data to be used
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if(!dbUserData[0]) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// DELETE /api/users/1
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if(dbUserData) {
+            res.status(404).json({ message: 'No user found with this id'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
