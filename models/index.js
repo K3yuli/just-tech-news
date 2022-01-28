c4bbb24ad9afb903ca239c0cd234903a3b13eecf
Kenia Lizama-Gamez <ke.yulizama@gmail.com>
2022-01-28 10:18:15 -0500

    post

--- models/index.js
+++ models/index.js
@@ -2,4 +2,17 @@
 const User = require('./User');
 const Post = require("./Post");
 
+// creat association
+// This association creates the reference for the id column in the User model
+// to link to the corresponding foreign key pair, which is the user_id in the Post model
+User.hasMany(Post, {
+    foreignKer: 'user_id'
+});
+
+// reverse association
+// The constraint we impose here is that a post can belong to one user, but not many users
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
--- server.js
+++ server.js
@@ -13,6 +13,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // turn on connection to db and server
+// connection with false
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
+
+// If we change the value of the force property to true, then the database connection must sync with the model definitions and associations.
+// By forcing the sync method to true, we will make the tables re-create if there are any association changes.
+// sequelize.sync({ force: true }).then(() => {
+//     app.listen(PORT, () => console.log('Now listening'));
+// });
