8108420ab8f7dece80d1070ae8aba533505c20e4
Kenia Lizama-Gamez <ke.yulizama@gmail.com>
2022-02-02 19:46:30 -0500

    login/loggout

---
+++ public/javascript/login.js
@@ -0,0 +1,79 @@
+// error handling to the front end w/ ES6 feature
+// add the await keyword before the Promise
+async function signupFormHandler(event) {
+  event.preventDefault();
+
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (username && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'post',
+      body: JSON.stringify({
+        username,
+        email,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    // check the response status
+    if (response.ok) {
+      console.log('success');
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+// login request
+async function loginFormHandler(event) {
+  event.preventDefault();
+
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (email && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'post',
+      body: JSON.stringify({
+        email,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+
+
+// listen function for the submit event from the form - to handle the login submission
+function signupFormHandler(event) {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (username && email && password) {
+      fetch('/api/users', {
+        method: 'post',
+        body: JSON.stringify({
+          username,
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      }).then((response) => {console.log(response)})
+    }
+  }
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
